e5a5032a626535b8caa8b5001d1b19f8c8141bbc
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2018-07-30 16:33:58 -0300

    ajusta menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,10 +14,49 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $events)
     {
         //Ajuste erro de tamanho do string padrão
         Schema::defaultStringLength(191);
+
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MENU PRINCIPAL');
+            $event->menu->add([
+                'text' => 'Inicial',
+                'url' => '/',
+                'icon' => 'home'
+            ]);
+            $event->menu->add([
+                'text' => 'Cadastros',
+                'url' => '/cadastros',
+                'icon' => 'clipboard-list'
+
+            ]);
+            $event->menu->add([
+                'text' => 'Relatórios',
+                'url' => '/relatorios',
+                'icon' => 'list-ol'
+
+            ]);
+            $event->menu->add([
+                'text' => 'Sorteio',
+                'url' => '/sorteios',
+                'icon' => 'columns'
+
+            ]);
+            $event->menu->add([
+                'text' => 'Configurações',
+                'url' => '/configuracoes',
+                'icon' => 'cogs'
+
+            ]);
+            $event->menu->add([
+                'text' => 'Apresentação',
+                'url' => '/apresentacao',
+                'icon' => 'desktop'
+
+            ]);
+        });
     }
 
     /**
