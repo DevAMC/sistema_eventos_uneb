aa263191b13a686cef174562375576c9f2c9e472
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2018-07-31 23:21:04 -0300

    finalização relatórios

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Participante;
+use Illuminate\Support\Facades\DB;
 
 class RelatorioController extends Controller
 {
@@ -18,6 +20,19 @@ class RelatorioController extends Controller
     }
 
     public function view(){
-        return view('relatorio.home');
+
+        $participantes = Participante::where('id_evento', session('id_evento'))->get();
+        $participantes_presentes = Participante::join('presencas', 'presencas.id_participante', '=', 'participantes.id')
+            ->where('presencas.id_label_evento', session('id_label_evento'))->get();
+        $participantes_nao_presentes = Participante::whereNotIn('id', DB::table('presencas')
+            ->where('id_label_evento', session('id_label_evento'))
+            ->pluck('id_participante'))->get();
+
+        $participantes_sorteados = Participante::join('presencas', 'presencas.id_participante', '=', 'participantes.id')
+            ->join('sorteado_tabs', 'sorteado_tabs.id_participante', '=', 'participantes.id')
+            ->where('presencas.id_label_evento', session('id_label_evento'))->get();
+
+
+        return view('relatorio.home', compact('participantes', 'participantes_presentes', 'participantes_nao_presentes', 'participantes_sorteados'));
     }
 }
--- resources/views/relatorio/home.blade.php
+++ resources/views/relatorio/home.blade.php
@@ -23,11 +23,13 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach($participantes as $participante)
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{{$participante->identificador}}</td>
+                    <td>{{$participante->nome}}</td>
+                    <td>{{$participante->campo}}</td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
         
@@ -49,11 +51,41 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach($participantes_presentes as $participante)
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{{$participante->identificador}}</td>
+                    <td>{{$participante->nome}}</td>
+                    <td>{{$participante->campo}}</td>
                 </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
+    </div>
+</div>
+
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">Participantes não validados</h3>
+    </div>
+    <div class="panel-body">
+        
+        <table id="table_participantes_nao_validados" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Campo</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($participantes_nao_presentes as $participante)
+                <tr>
+                    <td>{{$participante->identificador}}</td>
+                    <td>{{$participante->nome}}</td>
+                    <td>{{$participante->campo}}</td>
+                </tr>
+                @endforeach
             </tbody>
         </table>
         
@@ -75,11 +107,13 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach($participantes_sorteados as $participante)
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{{$participante->identificador}}</td>
+                    <td>{{$participante->nome}}</td>
+                    <td>{{$participante->campo}}</td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
         
@@ -99,6 +133,11 @@
             }
         });
         $('#table_participantes_validados').DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+            }
+        });
+        $('#table_participantes_nao_validados').DataTable({
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
             }
