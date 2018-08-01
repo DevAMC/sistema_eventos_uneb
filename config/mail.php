dae35d2e97bbeff2961b8859e173d41df3540856
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2018-08-01 18:14:10 -0300

    ajusta dados padrões e-mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'nao_responda@adventistas.org'),
+        'name' => env('MAIL_FROM_NAME', 'Sistema de Eventos - UNeB'),
     ],
 
     /*
