4ea2cd2f11b52966f0cab0e12c3eaca2133683ee
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2018-07-31 23:12:03 -0300

    ajusta front e configurações do datatable

--- resources/views/relatorio/home.blade.php
+++ resources/views/relatorio/home.blade.php
@@ -13,7 +13,24 @@
         <h3 class="panel-title">Todos participantes</h3>
     </div>
     <div class="panel-body">
-        Panel content
+        
+        <table id="table_participantes" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Campo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
     </div>
 </div>
 
@@ -22,7 +39,24 @@
         <h3 class="panel-title">Participantes validados</h3>
     </div>
     <div class="panel-body">
-        Panel content
+        
+        <table id="table_participantes_validados" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Campo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
     </div>
 </div>
 
@@ -31,7 +65,24 @@
         <h3 class="panel-title">Participantes sorteados</h3>
     </div>
     <div class="panel-body">
-        Panel content
+        
+        <table id="table_participantes_sorteados" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Campo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
     </div>
 </div>
 
@@ -40,6 +91,24 @@
 
 @push('js')
 
+<script>
+    $(document).ready(function () {
+        $('#table_participantes').DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+            }
+        });
+        $('#table_participantes_validados').DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+            }
+        });
+        $('#table_participantes_sorteados').DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+            }
+        });
+    });
 </script>
 
 @endpush
